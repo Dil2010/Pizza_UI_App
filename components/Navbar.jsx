5db8ead3085366c1d4be9a043cd5f60efa86e40a
Dilan Udawattha <87587934+Dil2010@users.noreply.github.com>
2022-06-01 10:45:01 -0700

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -17,8 +17,7 @@ const Navbar = () => {
         <ul className={styles.list}>
           <li className={styles.listItem}>Homepage</li>
           <li className={styles.listItem}>Products</li>
-          <li className={styles.listItem}>Menu</li>
-          <Image src="/img/logo.png" alt="" width="160px" height="69px" />
+          <li className={styles.listItem}>Menu</li>          
           <li className={styles.listItem}>Events</li>
           <li className={styles.listItem}>Blog</li>
           <li className={styles.listItem}>Contact</li>
